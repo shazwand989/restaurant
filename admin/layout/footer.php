6211be59e774d3a9689e2dc25e559474cf88dd0a
Izwan Danial <93575615+shazwand989@users.noreply.github.com>
2024-07-02 16:44:32 +0800

    Admin Layout

---
+++ admin/layout/footer.php
@@ -0,0 +1,74 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">+60 11-1670 0813
+        Developed by <a href="http://wa.me/601116700813" target="_blank">~Qin</a>
+    </div>
+    <strong>© 2021 <a href="../index.php"><?= SITE_NAME ?></a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../assets/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets/dist/js/adminlte.min.js"></script>
+
+<script src="../assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../assets/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../assets/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../assets/plugins/jszip/jszip.min.js"></script>
+<script src="../assets/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../assets/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../assets/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../assets/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../assets/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- SweetAlert2 -->
+<script src="../assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="../assets/plugins/chart.js/Chart.4.4.3.js"></script>
+
+
+<!-- select2 -->
+<script src="../assets/plugins/select2/js/select2.full.min.js"></script>
+
+<script>
+    function logout() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You will be logged out',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, logout',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            // Check if the user confirmed the logout
+            if (result.isConfirmed) {
+                // Perform logout action
+                window.location.href = 'logout.php';
+            }
+        });
+    }
+
+    function swal_alert(title, text, icon, location = '') {
+        Swal.fire({
+            title: title,
+            text: text,
+            icon: icon,
+            confirmButtonText: 'OK'
+        }).then((result) => {
+            if (location != '') {
+                window.location.href = location;
+            }
+        });
+    }
+</script>
+</body>
+
+</html>
---
+++ admin/layout/sidebar.php
@@ -0,0 +1,76 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+     <!-- Brand Logo -->
+     <a href="index.php" class="brand-link">
+         <img src="../assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+         <span class="brand-text font-weight-light">Admin Panel</span>
+     </a>
+
+     <!-- Sidebar -->
+     <div class="sidebar">
+         <!-- Sidebar user panel (optional) -->
+         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+             <div class="image">
+                 <img src="../assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+             </div>
+             <div class="info">
+                 <a href="#" class="d-block">Admin</a>
+             </div>
+         </div>
+
+         <!-- Sidebar Menu -->
+         <nav class="mt-2">
+             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                 <li class="nav-item">
+                     <a href="index.php" class="nav-link <?= $title == 'Dashboard' ? 'active' : '' ?>">
+                         <i class="nav-icon fas fa-tachometer-alt"></i>
+                         <p>
+                             Dashboard
+                         </p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="#" class="nav-link ">
+                         <i class="nav-icon fas fa-user"></i>
+                         <p>
+                             Users
+                             <i class="right fas fa-angle-left"></i>
+                         </p>
+                     </a>
+                     <ul class="nav nav-treeview">
+                         <li class="nav-item">
+                             <a href="user-list.php?role=admin" class="nav-link">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>Admin</p>
+                             </a>
+                         </li>
+                         <li class="nav-item">
+                             <a href="user-list.php?role=staff" class="nav-link">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>Staff</p>
+                             </a>
+                         </li>
+                     </ul>
+                 </li>
+                 <li class="nav-item">
+                     <a href="category-list.php" class="nav-link <?= $title == 'Category' ? 'active' : '' ?>">
+                         <i class="nav-icon fas fa-list"></i>
+                         <p>
+                             Category
+                         </p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="logout.php" class="nav-link">
+                         <i class="nav-icon fas fa-sign-out-alt"></i>
+                         <p>
+                             Logout
+                         </p>
+                     </a>
+                 </li>
+             </ul>
+         </nav>
+         <!-- /.sidebar-menu -->
+     </div>
+     <!-- /.sidebar -->
+ </aside>
